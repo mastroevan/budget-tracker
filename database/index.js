6ddaaed4f4b89cfee3008131848ab6aac63a99d3
mastroevan <mastroevan@gmail.com>
2021-12-22 12:06:31 -0700

    wrote queries and created config for mysql connection

---
+++ database/index.js
@@ -0,0 +1,31 @@
+const mysql = require('mysql');
+const mysqlConfig = require('./config.js');
+const connection = mysql.createConnection(mysqlConfig);
+
+connection.connect((err) => {
+  if (err) throw err;
+})
+
+const getAllReciepts = function(callback) {
+  connection.query(`Select * from transactions`, (err, data) => {
+    if (err) throw err;
+    console.log(data);
+    callback(data);
+  })
+};
+
+const getCategories = function(callback) {
+  connection.query('Select * from categories', (err, data) => {
+    if (err) throw err;
+    console.log(data);
+    callback(data);
+  })
+};
+
+const addCategory = function(values, callback) {
+  connection.query('INSERT INTO categories (description, amount) VALUES (?, ?)', values, (err, data) => {
+    if (err) throw err;
+    console.log(data);
+    callback(data);
+  })
+};
