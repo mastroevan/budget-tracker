ef923d1a19b8227d39d4ae03e51aca368fa5a45d
mastroevan <mastroevan@gmail.com>
2021-12-26 14:42:25 -0700

    MVP

--- client/src/components/TransactionList.jsx
+++ client/src/components/TransactionList.jsx
@@ -1,6 +1,6 @@
-import React from 'react';
+import React from "react";
 
-const TransactionList = ({transactions, categories}) => {
+const TransactionList = ({ transactions, categories }) => {
   return (
     <div className="txn">
       <h3>Transactions</h3>
@@ -9,34 +9,42 @@ const TransactionList = ({transactions, categories}) => {
           <div className="txn-data">Date</div>
           <div className="txn-data">Description</div>
           <div className="txn-data">Amount</div>
-          {categories.length !== 0 ? (<div className="txn-data">Category</div>) : null}
+          {categories.length !== 0 ? (
+            <div className="txn-data">Category</div>
+          ) : null}
         </div>
-        {
-          transactions.map((transaction) => {
-            return (
-              <div className="txn-row" key={transaction.id}>
-                <div className="txn-data" className="date">{transaction.date.toString().slice(0, 10)}</div>
-                <div className="txn-data" className="description">{transaction.description}</div>
-                <div className="txn-data" className="amount">{transaction.amount}</div>
-                <div className='txn-data' className="category">
-                    { categories.length !== 0 ? (
-                        categories.map((category) => {
-                          return(
-                            <select>
-                            <option value="none" selected disabled hidden>Add a category</option>
-                            <option className='category-title'>{category}</option>
-                           </select>
-                          )
-                        })) : null
-                    }
-                </div>
+        {transactions.map((transaction) => {
+          return (
+            <div className="txn-row" key={transaction.id}>
+              <div className="txn-data" className="date">
+                {transaction.date.toString().slice(0, 10)}
               </div>
-            )
-          })
-        }
+              <div className="txn-data" className="description">
+                {transaction.description}
+              </div>
+              <div className="txn-data" className="amount">
+                {transaction.amount}
+              </div>
+              <div className="txn-data" className="category">
+                <select>
+                  <option value="none" selected disabled hidden>
+                    Add a category
+                  </option>
+                  {categories.length !== 0
+                    ? categories.map((category) => {
+                        return (
+                          <option className="category-title">{category}</option>
+                        );
+                      })
+                    : null}
+                </select>
+              </div>
+            </div>
+          );
+        })}
       </div>
     </div>
-  )
+  );
 };
 
 export default TransactionList;
